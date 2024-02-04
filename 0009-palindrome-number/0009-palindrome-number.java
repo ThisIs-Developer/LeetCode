fbb4a424d1f9522532b6794da4bc2e8b33bad2cb
Baivab Sarkar <109382325+ThisIs-Developer@users.noreply.github.com>
2024-02-04 12:54:54 +0530

    Time: 5 ms (81.23%), Space: 43.8 MB (67.05%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -6,9 +6,8 @@ class Solution {
         int ori = x;
         int rev = 0;
 
-        while (x > 0) {
-            int digit = x % 10;
-            rev = (rev * 10) + digit;
+        while (x != 0) {
+            rev = (rev * 10) + x % 10;
             x = x / 10;
         }
 
