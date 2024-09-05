cebe046836f4c5fcdab51516147bcc9258076b95
Baivab Sarkar <109382325+ThisIs-Developer@users.noreply.github.com>
2024-09-06 01:16:12 +0530

    Time: 752 ms (97.97%) | Memory: 0B (100.00%) - LeetSync

--- 577-employee-bonus/employee-bonus.sql
+++ 577-employee-bonus/employee-bonus.sql
@@ -1,6 +1,5 @@
-# Write your MySQL query statement below
 SELECT e.name, b.bonus
 FROM Employee e
 LEFT JOIN Bonus b
-ON e.empId=b.empId
-WHERE b.bonus<1000 OR b.bonus IS NULL
+ON e.empId = b.empId
+WHERE COALESCE(b.bonus, 0) < 1000;
