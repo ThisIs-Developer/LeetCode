a50337f60d5c155e85b7ce81b5156b5f0a5f0bd5
Baivab Sarkar <109382325+ThisIs-Developer@users.noreply.github.com>
2024-09-13 00:24:25 +0530

    Time: 6 ms (78.56%) | Memory: 56.7 MB (41.04%) - LeetSync

--- 2411-spiral-matrix-iv/spiral-matrix-iv.java
+++ 2411-spiral-matrix-iv/spiral-matrix-iv.java
@@ -1,36 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-
     public int[][] spiralMatrix(int m, int n, ListNode head) {
-        // Store the east, south, west, north movements in a matrix.
-        int i = 0, j = 0, cur_d = 0, movement[][] = {
-            { 0, 1 },
-            { 1, 0 },
-            { 0, -1 },
-            { -1, 0 },
-        };
-        int[][] res = new int[m][n];
-        for (int[] row : res) {
-            Arrays.fill(row, -1);
+        int left = 0; int right = n;
+        int top = 0; int bottom = m;
+
+        int[][] result = new int[m][n];
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                result[i][j] = -1;
+            }
         }
 
-        while (head != null) {
-            res[i][j] = head.val;
-            int newi = i + movement[cur_d][0], newj = j + movement[cur_d][1];
+        while(head != null) {
+            // 1. left to right
+            for(int i = left; i < right; i++) {
+                if(head == null)
+                    return result;
+                result[top][i] = head.val;
+                head  = head.next;
+            }
+            top++;
 
-            // If we bump into an edge or an already filled cell, change the
-            // direction.
-            if (
-                Math.min(newi, newj) < 0 ||
-                newi >= m ||
-                newj >= n ||
-                res[newi][newj] != -1
-            ) cur_d = (cur_d + 1) % 4;
-            i += movement[cur_d][0];
-            j += movement[cur_d][1];
+            // 2. top to bottom
+            for(int i = top; i < bottom; i++) {
+                if(head == null)
+                    return result;
+                result[i][right - 1] = head.val;
+                head  = head.next;
+            }
+            right--;
 
-            head = head.next;
+            // 3. right to left
+            for(int i = right - 1; i >= left; i--) {
+                if(head == null)
+                    return result;
+                result[bottom - 1][i] = head.val;
+                head  = head.next;
+            }
+            bottom--;
+
+            // 4. bottom to top
+            for(int i = bottom - 1; i >= top; i--) {
+                if(head == null)
+                    return result;
+                result[i][left] = head.val;
+                head  = head.next;
+            }
+            left++;
         }
-
-        return res;
+        return result;
     }
 }
