84dbe00983f9a8419e70749a28d9db4d2e3ce506
Baivab Sarkar <109382325+ThisIs-Developer@users.noreply.github.com>
2024-09-13 00:22:40 +0530

    Time: 19 ms (26.26%) | Memory: 45.7 MB (25.22%) - LeetSync

--- 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.java
+++ 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.java
@@ -1,7 +1,35 @@
-class Solution {
-  public int countConsistentStrings(String allowed, String[] words) {
-    return (int) Arrays.stream(words)
-        .filter(word -> word.matches(String.format("[%s]*", allowed)))
-        .count();
-  }
+public class Solution {
+    public int countConsistentStrings(String allowed, String[] words) {
+
+
+        // 입력한 문자열만 > 있는 횟수를 구하는 거임 , 다른거 말고
+        //일단 set에  allwed 의 값을 넣고
+        // for문으로 words 돌아서 >  조건문 작성
+        int result = 0;
+        boolean flag = false;
+        Set<String> set = new HashSet<>();
+        for (char c : allowed.toCharArray()) {
+            set.add(String.valueOf(c));
+        }
+
+        //스트링
+    
+
+            for (String s : words) {
+
+                for (char c : s.toCharArray()) {
+                    //값이 들어간다는건 set에 있는거 제외 다른게 있다
+                    if (!set.contains(String.valueOf(c))) {
+                        flag = false;
+                        break;
+                    } else {
+                        flag = true;
+                    }
+                }
+                if (flag == true)
+                    result++;
+            }
+
+        return result;
+    }
 }
