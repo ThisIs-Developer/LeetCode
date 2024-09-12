e1ee31800e809e66c8aef4fdc3c0d428012480d4
Baivab Sarkar <109382325+ThisIs-Developer@users.noreply.github.com>
2024-09-13 00:22:51 +0530

    Time: 13 ms (45.64%) | Memory: 45 MB (92.85%) - LeetSync

--- 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.java
+++ 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.java
@@ -1,35 +1,21 @@
-public class Solution {
+class Solution {
     public int countConsistentStrings(String allowed, String[] words) {
-
-
-        // 입력한 문자열만 > 있는 횟수를 구하는 거임 , 다른거 말고
-        //일단 set에  allwed 의 값을 넣고
-        // for문으로 words 돌아서 >  조건문 작성
-        int result = 0;
-        boolean flag = false;
-        Set<String> set = new HashSet<>();
-        for (char c : allowed.toCharArray()) {
-            set.add(String.valueOf(c));
+        Set<Character> st = new HashSet<>();
+        for (int x = 0; x < allowed.length(); x++) {
+            st.add(allowed.charAt(x));
         }
-
-        //스트링
-    
-
-            for (String s : words) {
-
-                for (char c : s.toCharArray()) {
-                    //값이 들어간다는건 set에 있는거 제외 다른게 있다
-                    if (!set.contains(String.valueOf(c))) {
-                        flag = false;
-                        break;
-                    } else {
-                        flag = true;
-                    }
-                }
-                if (flag == true)
-                    result++;
+        int c = 0;
+        for (int i = 0; i < words.length; i++) {
+            char arr[] = words[i].toCharArray();
+            boolean flag = true;
+            for (int j = 0; j < arr.length; j++) {
+                if (!st.contains((arr[j])))
+                    flag = false;
             }
-
-        return result;
+            if (flag) {
+                c++;
+            }
+        }
+        return c;
     }
 }
