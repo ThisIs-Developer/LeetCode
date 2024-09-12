0b0a5a5f3baf1b0a71fdc929d5b58765c2de46d8
Baivab Sarkar <109382325+ThisIs-Developer@users.noreply.github.com>
2024-09-13 00:22:07 +0530

    Time: 1 ms (100.00%) | Memory: 44.9 MB (45.59%) - LeetSync

--- 2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.java
+++ 2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.java
@@ -1,14 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-  public ListNode insertGreatestCommonDivisors(ListNode head) {
-    for (ListNode curr = head; curr.next != null;) {
-      ListNode inserted = new ListNode(gcd(curr.val, curr.next.val), curr.next);
-      curr.next = inserted;
-      curr = inserted.next;
-    }
-    return head;
-  }
+    public ListNode insertGreatestCommonDivisors(ListNode head) {
+        
+        if(head.next == null) return head;
 
-  private int gcd(int a, int b) {
-    return b == 0 ? a : gcd(b, a % b);
-  }
+        ListNode node1 = head;
+        ListNode node2 = head.next;
+
+        while(node2 != null){
+
+            int gcd = calculateGCD(node1. val, node2.val);
+            ListNode gcdNode = new ListNode(gcd);
+
+            node1.next = gcdNode;
+            gcdNode.next = node2;
+
+            node1 = node2;
+            node2 = node2.next;
+
+        }
+
+        return head;
+
+    }
+
+    public int calculateGCD(int num1, int num2){
+
+        while(num2 != 0){
+
+            int temp = num2;
+            num2 = num1 % num2;
+            num1 = temp;
+
+        }
+
+        return num1;
+
+    }
+
 }
