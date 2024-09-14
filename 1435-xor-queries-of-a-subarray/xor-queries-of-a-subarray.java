bd40e093f92300dd644b5885bb4b91202f2c2755
Baivab Sarkar <109382325+ThisIs-Developer@users.noreply.github.com>
2024-09-14 21:38:36 +0530

    Time: 19 ms (25.32%) | Memory: 56.4 MB (71.97%) - LeetSync

--- 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.java
+++ 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.java
@@ -1,23 +1,53 @@
 class Solution {
     public int[] xorQueries(int[] arr, int[][] queries) {
+        int n = arr.length;
+        int q = queries.length;
+        int blockSize = (int) Math.sqrt(n);
+        int[] answer = new int[q];
         
-        int ans[]= new int[queries.length];
-        int ind=0;
-        for(int[] arrr: queries){
-             int xor = 0;
-            int left = arrr[0];
-            int right = arrr[1];
-            for(int i=left; i<=right; i++){
-                xor = xor ^ arr[i];
-
-            }
-            System.out.println(xor);
-            ans[ind]=xor;
-            ind++;
-
+        Query[] qs = new Query[q];
+        for (int i = 0; i < q; i++) {
+            qs[i] = new Query(queries[i][0], queries[i][1], i);
         }
-         return ans;
+        
+        Arrays.sort(qs, (a, b) -> {
+            int blockA = a.left / blockSize;
+            int blockB = b.left / blockSize;
+            if (blockA != blockB) return blockA - blockB;
+            return a.right - b.right;
+        });
+        
+        int currentLeft = 0, currentRight = -1, currentXor = 0;
+        
+        for (Query query : qs) {
+            while (currentRight < query.right) {
+                currentRight++;
+                currentXor ^= arr[currentRight];
+            }
+            while (currentRight > query.right) {
+                currentXor ^= arr[currentRight];
+                currentRight--;
+            }
+            while (currentLeft < query.left) {
+                currentXor ^= arr[currentLeft];
+                currentLeft++;
+            }
+            while (currentLeft > query.left) {
+                currentLeft--;
+                currentXor ^= arr[currentLeft];
+            }
+            answer[query.index] = currentXor;
+        }
+        
+        return answer;
     }
-
-   
+    
+    class Query {
+        int left, right, index;
+        Query(int l, int r, int i) {
+            left = l;
+            right = r;
+            index = i;
+        }
+    }
 }
