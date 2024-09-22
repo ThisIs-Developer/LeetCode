b9cf41ab97d052cbbbdc527ddf07f41e461e842c
Baivab Sarkar <109382325+ThisIs-Developer@users.noreply.github.com>
2024-09-22 14:07:23 +0530

    Time: 17 ms (6.31%) | Memory: 56.7 MB (55.13%) - LeetSync

--- 2411-spiral-matrix-iv/spiral-matrix-iv.java
+++ 2411-spiral-matrix-iv/spiral-matrix-iv.java
@@ -1,104 +1,21 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
-    // my code 
-    // public void fillup(int[][] arr,int i,int j,ListNode head){
-    //     if(head!=null){
-    //         arr[i][j] = head.val;
-    //     }
-    //     else{
-    //         arr[i][j] = -1;
-    //     }
-    // } 
-    // public int[][] spiralMatrix(int m, int n, ListNode head) {
-    //     int left =0;
-    //     int right = n-1;
-    //     int top =0;
-    //     int bottom = m-1;
-    //     int[][] result = new int[m][n];
-    //     while(left<=right && top <=bottom){
-           
-    //         for(int j=left; j<=right;j++){
-    //             fillup(result,top,j,head);
-    //             if(head != null){
-    //                 head = head.next;
-    //             }
-    //         }
-    //         top++; 
-    //         for(int i=top; i<=bottom;i++){
-    //             fillup(result,i,right,head);
-    //             if(head != null){
-    //                 head = head.next;
-    //             }
-    //         } 
-    //         right--;
-    //         if (top <= bottom) {
-    //         for(int j =right; j>=left;j--){
-    //             fillup(result,bottom,j,head);
-    //             if(head != null){
-    //                 head = head.next;
-    //             }
-    //         } 
-    //         bottom--;
-    //         }
-    //         if (left <= right) {
-    //         for(int i=bottom; i>=top;i--){
-    //             fillup(result,i,left,head);
-    //             if(head != null){
-    //                 head = head.next;
-    //             }
-    //         } 
-    //         left++;
-    //         }
-    //     }
-    //     return result; 
-    
-    // better code
-    public int[][] spiralMatrix(int m, int n, ListNode head) {
-        int left =0;
-        int right = n-1;
-        int top =0;
-        int bottom = m-1;
-        int[][] arr = new int[m][];
-        for(int i=0; i<m;i++){
-            arr[i] = new int[n];
-            Arrays.fill(arr[i],-1);
-        }
-        while(head != null){
-           
-            for(int j=left; j<=right && head != null;j++){
-                arr[top][j] = head.val;
-                head = head.next;
-            }
-            top++; 
-            for(int i=top; i<=bottom && head != null ;i++){
-                arr[i][right] = head.val;
-                head = head.next;
-            } 
-            right--;
-            if (top <= bottom) {
-            for(int j =right; j>=left && head != null;j--){
-                arr[bottom][j] = head.val;
-                head = head.next;
-            } 
-            bottom--;
-            }
-            if (left <= right) {
-            for(int i=bottom; i>=top && head != null;i--){
-                arr[i][left] = head.val;
-                head = head.next;
-            } 
-            left++;
-            }
-        }
-        return arr; 
+  public int[][] spiralMatrix(int m, int n, ListNode head) {
+    final int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    int[][] ans = new int[m][n];
+    Arrays.stream(ans).forEach(A -> Arrays.fill(A, -1));
+    int x = 0; // the current x position
+    int y = 0; // the current y position
+    int d = 0;
+
+    for (ListNode curr = head; curr != null; curr = curr.next) {
+      ans[x][y] = curr.val;
+      if (x + dirs[d][0] < 0 || x + dirs[d][0] == m || y + dirs[d][1] < 0 || //
+          y + dirs[d][1] == n || ans[x + dirs[d][0]][y + dirs[d][1]] != -1)
+        d = (d + 1) % 4;
+      x += dirs[d][0];
+      y += dirs[d][1];
     }
+
+    return ans;
+  }
 }
