e77dae0b5b54dee82ae177c790f60ea9d36fe709
Baivab Sarkar <109382325+ThisIs-Developer@users.noreply.github.com>
2024-09-22 14:09:04 +0530

    Time: 4 ms (99.96%) | Memory: 56.9 MB (20.76%) - LeetSync

--- 2411-spiral-matrix-iv/spiral-matrix-iv.java
+++ 2411-spiral-matrix-iv/spiral-matrix-iv.java
@@ -1,39 +1,41 @@
 class Solution {
     public int[][] spiralMatrix(int m, int n, ListNode head) {
-        int colStart = 0, colEnd = n - 1, rowStart = 0, rowEnd = m - 1;
-        int[][] res = new int[m][n];
-        for(int i = 0; i < m; i++) {
-            Arrays.fill(res[i], -1);
+        int ans[][]=new int[m][];
+        for(int i=0;i<m;i++){
+            int res[]=new int[n];
+            Arrays.fill(res,-1);
+            ans[i]=res;
         }
-        while(colStart <= rowEnd) {
-            for(int i = colStart; i <= colEnd; i++) {
-                if(head != null) {
-                    res[rowStart][i] = head.val;
-                    head = head.next;
-                }   
+        
+        int rmin = 0;
+        int rmax = m-1;
+        int cmin = 0;
+        int cmax = n-1;
+
+       while(head!=null){
+            
+            for(int c=cmin;c<=cmax && head!=null;c++){
+                ans[rmin][c] = head.val;
+                head = head.next;
             }
-            for(int i = rowStart + 1; i <= rowEnd; i++) {
-                if(head != null) {
-                    res[i][colEnd] = head.val;
-                    head = head.next;
-                }
+            rmin++;
+            for(int r=rmin;r<=rmax && head!=null;r++){
+                ans[r][cmax] = head.val;
+                head = head.next;
             }
-            for(int i = colEnd - 1; i >= colStart; i--) {
-                if(head != null) {
-                    res[rowEnd][i] = head.val;
-                    head = head.next;
-                }
+            cmax--;
+            for(int c=cmax;c>=cmin && head!=null;c--){
+                ans[rmax][c] = head.val;
+                head = head.next;
             }
-            for(int i = rowEnd - 1; i >= rowStart + 1; i--) {
-                if(head != null) {
-                    res[i][colStart] = head.val;
-                    head = head.next;
-                }
+            rmax--;
+
+            for(int r=rmax;r>=rmin && head!=null;r--){
+                ans[r][cmin] = head.val;
+                head = head.next;
             }
-            if(head == null)
-                break;
-            colStart++; colEnd--; rowStart++; rowEnd--;
-        }
-        return res;
+            cmin++;
+        } 
+        return ans;
     }
 }
