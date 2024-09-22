52f46d2442ba0c735f7a1b77d07eff0c13cfd7be
Baivab Sarkar <109382325+ThisIs-Developer@users.noreply.github.com>
2024-09-22 14:08:04 +0530

    Time: 9 ms (28.50%) | Memory: 56.3 MB (89.98%) - LeetSync

--- 2411-spiral-matrix-iv/spiral-matrix-iv.java
+++ 2411-spiral-matrix-iv/spiral-matrix-iv.java
@@ -1,64 +1,57 @@
 class Solution {
     public int[][] spiralMatrix(int m, int n, ListNode head) {
-        int[][] result = new int[m][n];
-        List<Integer> aux = new ArrayList<>();
-        String[] actions = { "Right", "Down", "Left", "Up" };
-        int index = 0, i = 0, j = 0, actionsIter = 0, k = 0;
-        int left = 0, right = n - 1, top = 0, bottom = m - 1;
+        int[][] res = new int[m][n];
 
-        while (head != null) {
-            aux.add(head.val);
-            head = head.next;
+        for (int[] arr : res) {
+            Arrays.fill(arr, -1);
         }
+        
 
-        for (i = 0; i < m; i++) {
-            for (j = 0; j < n; j++) {
-                result[i][j] = -1;
+        int rowUp = 0, rowDown = m - 1, colLeft = 0, colRight = n - 1;
+
+        while (rowUp <= rowDown && colLeft <= colRight) {
+
+            for (int i = colLeft; i <= colRight; i++) {
+                res[rowUp][i] = head == null ? -1 : head.val;
+                head = head == null ? null : head.next;
             }
-        }
+            rowUp++;
 
-        i = 0;
-        j = 0;
-
-        while (k < aux.size()) {
-            if (actions[actionsIter].equals("Right")) {
-                for (j = left; j <= right && k < aux.size(); j++) {
-                    result[top][j] = aux.get(k);
-                    k++;
-                }
-
-                top++;
-                actionsIter++;
-
-            } else if (actions[actionsIter].equals("Down")) {
-                for (i = top; i <= bottom && k < aux.size(); i++) {
-                    result[i][right] = aux.get(k);
-                    k++;
-                }
-
-                right--;
-                actionsIter++;
-
-            } else if (actions[actionsIter].equals("Left")) {
-                for (j = right; j >= left && k < aux.size(); j--) {
-                    result[bottom][j] = aux.get(k);
-                    k++;
-                }
-
-                bottom--;
-                actionsIter++;
-
-            } else if (actions[actionsIter].equals("Up")) {
-                for (i = bottom; i >= top && k < aux.size(); i--) {
-                    result[i][left] = aux.get(k);
-                    k++;
-                }
-
-                left++;
-                actionsIter = 0;
+            if (rowUp > rowDown) {
+                break;
             }
-        }
 
-        return result;
+            for (int i = rowUp; i <= rowDown; i++) {
+                res[i][colRight] = head == null ? -1 : head.val;
+                head = head == null ? null : head.next;
+            }
+            colRight--;
+            
+            if (colLeft > colRight) {
+                break;
+            }
+
+            for (int i = colRight; i >= colLeft; i--) {
+                res[rowDown][i] = head == null ? -1 : head.val;
+                head = head == null ? null : head.next;
+            }
+            rowDown--;
+
+            if (rowUp > rowDown) {
+                break;
+            }
+
+            for (int i = rowDown; i >= rowUp; i--) {
+                res[i][colLeft] = head == null ? -1 : head.val;
+                head = head == null ? null : head.next;
+            }
+            colLeft++;
+            
+            if (colLeft > colRight) {
+                break;
+            }
+
+        }
+        return res;
     }
 }
