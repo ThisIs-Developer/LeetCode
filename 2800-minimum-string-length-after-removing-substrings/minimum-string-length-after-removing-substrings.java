7c996cd4bf8596f83bf2423b3daf07c48b90ed41
Baivab Sarkar <109382325+ThisIs-Developer@users.noreply.github.com>
2024-10-08 11:47:39 +0530

    Time: 1 ms (100.00%) | Memory: 43.4 MB (49.96%) - LeetSync

--- 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
@@ -1,11 +1,14 @@
 class Solution {
     public int minLength(String s) {
-        StringBuilder sb = new StringBuilder();
-        for(char c:s.toCharArray()){
-            if(c=='B' && sb.length()>0 && sb.charAt(sb.length()-1)=='A') sb.delete(sb.length()-1,sb.length());
-            else if(c=='D' && sb.length()>0 && sb.charAt(sb.length()-1)=='C') sb.delete(sb.length()-1,sb.length());
-            else sb.append(c);
+        int n = s.length(), p = -1;
+        char[] stack = new char[n];
+        for (char c : s.toCharArray()) {
+            if (p >= 0 && ((c == 'B' && stack[p] == 'A') || (c == 'D' && stack[p] == 'C'))) {
+                p--;
+            } else {
+                stack[++p] = c;
+            }
         }
-        return sb.length();
+        return p + 1;
     }
 }
