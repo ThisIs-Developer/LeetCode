924129f45af83856fff91fb3b291d8e114a00296
Baivab Sarkar <109382325+ThisIs-Developer@users.noreply.github.com>
2024-10-08 11:46:24 +0530

    Time: 16 ms (8.20%) | Memory: 45.2 MB (10.81%) - LeetSync

--- 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
@@ -1,19 +1,28 @@
 class Solution {
-  public int minLength(String s) {
-    Deque<Character> stack = new ArrayDeque<>();
-
-    for (final char c : s.toCharArray())
-      if (c == 'B' && match(stack, 'A'))
-        stack.pop();
-      else if (c == 'D' && match(stack, 'C'))
-        stack.pop();
-      else
-        stack.push(c);
-
-    return stack.size();
-  }
-
-  private boolean match(Deque<Character> stack, char c) {
-    return !stack.isEmpty() && stack.peek() == c;
-  }
+    public int minLength(String s) {
+        String st=s;
+        while(st.indexOf("AB")>=0 || st.indexOf("CD")>=0)
+        {
+            int idx=0;
+            if(st.indexOf("AB")>=0){
+            idx=st.indexOf("AB");
+            }
+            else if(st.indexOf("CD")>=0)
+            {
+                idx=st.indexOf("CD");
+            }
+            else
+            {
+                break;
+            }
+             if((idx+2)<=st.length()-1)
+            st=st.substring(0,idx)+st.substring(idx+2);
+            else
+            {
+             st=st.substring(0,idx);
+            }
+        }
+        System.out.println(st);
+        return st.length();
+    }
 }
