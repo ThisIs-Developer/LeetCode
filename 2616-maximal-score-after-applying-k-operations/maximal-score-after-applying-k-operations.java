cb9a1d1021f8bdc0df0930e9b74a479525d8fe00
Baivab Sarkar <109382325+ThisIs-Developer@users.noreply.github.com>
2024-10-14 18:59:44 +0530

    Time: 154 ms (46.92%) | Memory: 63 MB (60.66%) - LeetSync

--- 2616-maximal-score-after-applying-k-operations/maximal-score-after-applying-k-operations.java
+++ 2616-maximal-score-after-applying-k-operations/maximal-score-after-applying-k-operations.java
@@ -1,16 +1,17 @@
 class Solution {
     public long maxKelements(int[] nums, int k) {
-        long score=0;
-        PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
-        for(int elem:nums){
-            pq.offer(elem);
-        }    
-        while(k>0){
-            int p=pq.poll();
-            score+=(long)p;
-            pq.add((int)Math.ceil(p/(double)3));
-            k--;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i=0;i<nums.length;i++){
+            pq.add(nums[i]);
         }
-        return score;
+        long ans = 0;
+        while(k-->0){
+            int num = pq.poll();
+            int x = num%3==0?num/3:num/3+1;
+            // System.out.println(num+" "+x);
+            ans+=num;
+            pq.add(x);
+        }
+        return ans;
     }
 }
