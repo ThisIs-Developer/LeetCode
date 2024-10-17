e4d013854239e893644c21f84bbdad3e9dfd2613
Baivab Sarkar <109382325+ThisIs-Developer@users.noreply.github.com>
2024-10-17 22:26:23 +0530

    Time: 2 ms (8.36%) | Memory: 40.2 MB (66.95%) - LeetSync

--- 670-maximum-swap/maximum-swap.java
+++ 670-maximum-swap/maximum-swap.java
@@ -1,19 +1,30 @@
 class Solution {
-  public int maximumSwap(int num) {
-    char[] s = Integer.toString(num).toCharArray();
-    int[] lastIndex = new int[10]; // {digit: last index}
-
-    for (int i = 0; i < s.length; ++i)
-      lastIndex[s[i] - '0'] = i;
-
-    for (int i = 0; i < s.length; ++i)
-      for (int d = 9; d > s[i] - '0'; --d)
-        if (lastIndex[d] > i) {
-          s[lastIndex[d]] = s[i];
-          s[i] = (char) ('0' + d);
-          return Integer.parseInt(new String(s));
+    public int maximumSwap(int number) {
+        int count[] = new int[10];
+        int lastPos[] = new int[10];
+        StringBuilder str = new StringBuilder();
+        str.append(number);
+        for(int i=0;i<str.length();i++) {
+            count[str.charAt(i)-'0']++;
+            lastPos[str.charAt(i)-'0'] = i;
         }
-
-    return num;
-  }
+        int i = 0;
+        for(int num=9;num>=0;num--) {
+            while(count[num]>0) {
+                if((str.charAt(i)-'0') == num) {
+                    count[num]--;
+                    i++;
+                } else {
+                    //System.out.println(num+" "+i+" "+lastPos[num]);
+                    char currChar = str.charAt(i);
+                    str.setCharAt(i, str.charAt(lastPos[num]));
+                    str.setCharAt(lastPos[num], currChar);
+                    num = -1;
+                    break;
+                }
+            }
+        }
+        System.out.println(str);
+        return new Integer(str.toString());
+    }
 }
